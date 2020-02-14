e9f2c148bef1547d2db966a274bf5deaba8e53bb
User Name <artur.it2914@gmail.com>
2020-02-14 18:15:53 +0200

    Refactor: entry point index.js
    add routing

--- src/index.js
+++ src/index.js
@@ -1,44 +1,10 @@
 const express = require( 'express' );
-const { UserController, TaskController } = require( './controllers' );
-
+const router = require( './routers' );
 const PORT = process.env.NODE_PORT || 3000;
 
 const app = express();
 app.use( express.json() );
 
-app.post( '/login',
-          UserController.getUserByEmail,
-          comparePassword,
-          generateTokenPair,
-          saveRefreshTokenToDb,
-          sendUser
-);
-
-app.post( '/user', UserController.createUser );
-
-app.get( '/user/:id', UserController.getUser );
-
-app.patch( '/user/:id', UserController.updateUser );
-
-app.delete( '/user/:id', UserController.deleteUser );
-
-app.post( '/Task', TaskController.createTask );
-
-app.get( '/Task/:id', TaskController.getTask );
-
-app.patch( '/Task/:id', TaskController.updateTask );
-
-app.delete( '/Task/:id', TaskController.deleteTask );
-
-app.use(
-  (req, res, next) => {
-    try {
-      res.send( req.data );
-
-    } catch (e) {
-      next( e );
-    }
-  }
-);
+app.use( router );
 
 app.listen( PORT, () => console.log( `Example app listening on port ${PORT}!` ) );
