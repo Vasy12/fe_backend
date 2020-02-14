a03ec461cb2a4a59d4ae7bb0127fb3f0abcb5c98
User Name <artur.it2914@gmail.com>
2020-02-14 18:16:29 +0200

    Fix: user controller
    rename find user method

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -28,29 +28,11 @@ class UserController {
   getUserById = async (req, res, next) => {
     try {
 
-      req.user = await this._controller.read( req.params.id, {
+      res.send( await this._controller.read( req.params.id, {
         attributes: {
           exclude: ['password']
         }
-      } );
-      next();
-    } catch (e) {
-      next( e );
-    }
-  };
-
-  getUserByEmail = async (req, res, next) => {
-    try {
-
-      req.user = await User.findOne( {
-                                       where: {
-                                         email: req.body.email,
-                                       }
-                                     } );
-      if (req.user) {
-        return next();
-      }
-      res.status( 404 ).send( `User with email "${req.body.email}" doesn't exist` );
+      } ) );
 
     } catch (e) {
       next( e );
