4f87ed6924d111e0926ca9f6d02cb497bfe4db13
User Name <artur.it2914@gmail.com>
2020-02-20 18:27:31 +0200

    Create: admin simple router

---
+++ src/routers/admin.js
@@ -0,0 +1,22 @@
+const express = require( 'express' );
+const { User } = require( './../models' );
+const adminRouter = express.Router();
+
+adminRouter.route( '/users' )
+           .get( async (req, res, next) => {
+             try {
+               const users = await User.findAll( {
+                                                   limit: req.query.limit || 20,
+                                                   offset: req.query.offset || 0,
+                                                   attributes: {
+                                                     exclude: ['password']
+                                                   }
+                                                 } );
+               res.send( users );
+             } catch (e) {
+               next( e );
+             }
+           } );
+
+module.exports = adminRouter;
+
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,16 @@ const express = require( 'express' );
 const { checkAuthorization } = require( '../middlewares/authorization' );
 const { ApplicationError } = require( '../utils/errors' );
 const { UserController, TaskController } = require( '../controllers' );
-
+const adminRouter = require( './admin.js' );
 const router = express.Router();
 
-router.use( checkAuthorization );
+/*router.use( checkAuthorization );*/
+
+router.use( '/admin', adminRouter );
+
+router.route( '/users' )
+      .get( UserController.getAllUsers );
+
 router.route( '/user(/:id)?' )
       .post( UserController.createUser )
       .get( UserController.getUserById )
