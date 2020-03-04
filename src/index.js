0221464a52f9ba5970002403dc2846a7b9a935c8
User Name <artur.it2914@gmail.com>
2020-03-04 21:14:51 +0200

    Add: all

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ app.use( express.json() );
 
 app.use( express.static( path.resolve( __dirname, '../public' ) ) );
 
+app.post( '/api/refresh', (req, res, next) => {
+  res.send( '6' );
+} );
+
 app.use( '/api', router );
 
 app.listen( PORT, () => console.log( `Example app listening on port ${PORT}!` ) );
